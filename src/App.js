894a3a9ccf47ac1ce2b9f83d02321a5158c73a9d
dmjendor@gmail.com <dmjendor@gmail.com>
2025-12-17 10:06:25 -0500

    Example of passing elements as props as an alternative to children

--- src/App.js
+++ src/App.js
@@ -62,6 +62,16 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
+        {/* passing implicitly 
+        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList watched={watched} />
+            </>
+          }
+        />*/}
         <Box>
           <MovieList movies={movies} />
         </Box>
